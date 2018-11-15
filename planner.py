c6c2f3827b017d94465c1025ee92572337dad075
Brandon Rhodes <brandon@rhodesmill.org>
2018-11-15 07:58:57 -0500

    Add other major trails, and more routes

--- planner.py
+++ planner.py
@@ -18,27 +18,33 @@ routes = [[
 
 # https://www.nps.gov/grca/planyourvisit/upload/tonto_distances.pdf
 
+# TODO: Page Spring water
+# TODO: Lower Boucher trail?
+
 some_input = """
 A9 Little Colorado
-9.1 BB9 Tanner Beach
+6.5 Lava Canyon Rapids
+3.0 BB9 Tanner Beach
 3.0 BC9 Cardenas Creek
-7.0 BC9 Nevills Rapids
-2.0 BD9 Hance Rapids
-6.5 BE9 Hance Creek
-1.9 BF5 Horseshoe Mesa
-1.5 BG9 Cottonwood Creek
+4.3 Escalante Creek
+2.3 Seventyfivemile Creek
+2.4 BD9 Hance Rapids
+6.5 BE9 Hance Creek water
+1.9 BF5 Horseshoe Mesa toilet camp
+1.5 BG9 Cottonwood Creek water?
 5.5 BH9 Grapevine Creek
 8.7 BJ9 Lonetree Canyon
 3.5 BJ9 Cremation Creek
-4.8 CIG Indian Garden water camp
-2.5 BL4 Horn Creek camp
-4.8 BL5 Salt Creek camp
+2.1 Tip Off
+2.7 CIG Indian Garden water toilet camp
+2.5 BL4 Horn Creek toilet camp
+4.8 BL5 Salt Creek toilet camp
 2.1 BL6 Cedar Spring water? camp
-1.3 BL7 Monument Creek water camp
+1.3 BL7 Monument Creek toilet water camp
 3.4 Hermit Tonto junction
-1.2 BM7 Hermit Creek water camp toilet
+1.2 BM7 Hermit Creek water toilet camp
 6.2 Boucher Tonto junction
-0.4 BN9 Boucher Upper Creek Crossing water
+0.4 BN9 Boucher Upper Creek Crossing water camp
 5.7 BO9 Slate Creek
 9.3 BO9 Turquoise Creek
 5.8 BP9 Ruby Creek
@@ -74,13 +80,49 @@ Hermit Trailhead
 
 Hermit Trailhead
 2.7 Boucher Hermit Junction
-2.5 Yuma Point
-2.4 Boucher top of Redwall
+2.5 Yuma Point camp
+2.4 Boucher top of Redwall camp
 1.3 Boucher Tonto junction
+
+CIG Indian Garden
+3.2 River Resthouse water toilet
+1.5 Bright Angel Campground water toilet camp
+0.4 Phantom Ranch water toilet
+
+South Kaibab Trailhead
+1.5 Cedar Ridge toilet
+1.5 Skeleton Point toilet
+1.4 Tip Off toilet
+2.6 Bright Angel Campground
+
+North Kaibab Trailhead
+1.7 Supai Tunnel water?
+3.0 Roaring Springs water?
+0.7 Manzanita Rest Area
+1.4 Cottonwood Campground water? camp
+1.6 Ribbon Falls
+4.9 North Kaibab Clear Creek junction
+0.3 Phantom Ranch
+
+North Kaibab Trailhead
+1.5 North Rim Campground water? camp
+
+North Kaibab Clear Creek junction
+1.7 Sumner Wash camp
+6.7 Clear Creek toilet water camp
+6.0 Clear Creek at Colorado River
+
+Clear Creek
+5.0 Cheyava Falls
+"""
+
+some_input += """
+Bright Angel Campground
+2.0 Utah Flats
+2.0 Phantom Creek
 """
 
 water = {'b', 'e'}
-camping = {'c', 'e'}
 # mileages = [
 #     ('a', 'b', 3.0),
 #     ('b', 'c', 1.5),
@@ -89,7 +131,7 @@ camping = {'c', 'e'}
 # ]
 attribute_words = {'camp', 'toilet', 'water', 'water?'}
 HEADING = '''
-miles miles miles since
+miles miles miles since  Day {}
       today total water
 '''.strip()
 
@@ -98,7 +140,7 @@ def main(argv):
     # parser.add_argument('waypoints', nargs='+', help='waypoint codes')
     # args = parser.parse_args(argv)
 
-    attributes = {}  # {(waypoint, attr)}
+    attributes = {}  # {waypoint: {attr)}
     mileages = {}    # {w1: {w2: mileage}}
 
     for line in some_input.splitlines():
@@ -107,7 +149,8 @@ def main(argv):
             continue
         if not line[0].isdigit():
             here = line
-            attributes[here] = set()
+            if here not in attributes:
+                attributes[here] = set()
             continue
         words = line.split()
         miles = float(words[0])
@@ -123,9 +166,12 @@ def main(argv):
         mileages[here][there] = miles
         mileages[there][here] = miles
         here = there
-        attributes[here] = these_attributes
+        if here in attributes:
+            attributes[here] = attributes[here] | these_attributes
+        else:
+            attributes[here] = these_attributes
 
-    pprint(mileages)
+    #pprint(mileages)
 
     # distances = {}
     # for start, finish, mileage in mileages:
@@ -134,19 +180,23 @@ def main(argv):
 
     waypoints = [
         'Hermit Trailhead',
-        'BN9 Boucher Upper Creek Crossing',
+        'Yuma Point',
+        'camp',
+        'BN9 Boucher Upper Creek Crossing',  # fix
         'camp',
         'BM7 Hermit Creek',
-        'BM8 Hermit Rapids',
+        'BM8 Hermit Rapids',  # camp at Tonto? or river?
         'camp',
         'BL8 Granite Rapids',
+        'camp',
+        'BL5 Salt Creek',  # bad idea? BL6 instead?
         'camp',
         'BL4 Horn Creek',
         'camp',
         'Bright Angel Trailhead',
     ]
 
-    waypoints = [
+    waypoints2 = [  # TODO: study where campgrounds are
         'BB9 Tanner Trailhead',
         'BB9 Tanner Beach',
         'camp',
@@ -156,31 +206,68 @@ def main(argv):
         'camp',
         'BC9 Cardenas Creek',
         'camp',
-        'BD9 Hance Rapids',
+        'Seventyfivemile Creek',
         'camp',
-        'BE9 Hance Creek',
+        'BE9 Hance Creek',  # water?
         'camp',
-        'Grandview Trailhead',
+        'Grandview Trailhead',  # how get back?
+    ]
+
+    waypoints2 = [
+        'South Kaibab Trailhead',
+        'Cottonwood Campground',
+        'camp',
+        'Cottonwood Campground',
+        'camp',
+        'Utah Flats',
+        'camp',
+        'Phantom Creek',
+        'camp',
+        'Utah Flats',
+        'camp',
+        'Bright Angel Campground',
+        'camp',
+        'Bright Angel Trailhead'
+    ]
+
+    waypoints2 = [
+        'South Kaibab Trailhead',
+        'Cottonwood Campground',
+        'camp',
+        #'Bright Angel Campground',
+        'Phantom Ranch',
+        'Sumner Wash',
+        'camp',
+        'Clear Creek',
+        'camp',
+        'Cheyava Falls',
+        'camp',
+        'Clear Creek',
+        'camp',
+        'Bright Angel Campground',
+        'camp',
+        'Bright Angel Trailhead'
     ]
 
     waypoints = list(expand_waypoints(waypoints, mileages))
-    pprint(waypoints)
+    #pprint(waypoints)
 
     day = 1
     miles = 0.0
     miles_today = 0.0
     miles_since_water = 0.0
-    print('Day 1')
-    print('Starting at', waypoints[0])
-    print(HEADING)
+    line_format = '{:5.1f} {:5.1f} {:5.1f} {:5.1f}  {}'
+    print(HEADING.format(1))
+    print(line_format.format(0.0, 0.0, 0.0, 0.0, waypoints[0]))
     last_waypoint = waypoints[0]
     for waypoint in waypoints[1:]:
         #print('*', waypoint)
         if waypoint == 'camp':
+            if 'camp' not in attributes[last_waypoint]:
+                print('WARNING: no campground')
             print()
             day = day + 1
-            print('Day {}'.format(day))
-            print(HEADING)
+            print(HEADING.format(day))
             miles_today = 0.0
             continue
         new_miles = mileages[last_waypoint][waypoint]
@@ -188,15 +275,15 @@ def main(argv):
         miles_today += new_miles
         miles_since_water += new_miles
         words = [waypoint]
-        if waypoint in camping:
-            words.append('camping')
+        if 'camp' in attributes[waypoint]:
+            words.append('camp')
         #if waypoint in water:
+        print(line_format.format(
+            new_miles, miles_today, miles, miles_since_water, ' '.join(words)
+        ))
         if 'water' in attributes[waypoint]:
             words.append('water')
             miles_since_water = 0.0
-        words = ' '.join(words)
-        print('{:5.1f} {:5.1f} {:5.1f} {:5.1f} {}'.format(
-            new_miles, miles_today, miles, miles_since_water, words))
         last_waypoint = waypoint
 
 def expand_waypoints(waypoints, mileages):
