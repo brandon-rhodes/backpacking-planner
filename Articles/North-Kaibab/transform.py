5a3d086a4c04b74dd5813aa2ea90d600bf92f662
Brandon Rhodes <brandon@rhodesmill.org>
2021-01-19 08:23:21 -0500

    Try modifying the SVG programmatically

---
+++ Articles/North-Kaibab/transform.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+NORTH = 36.188
+SOUTH = 1
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Transform SVG using Python')
+    parser.parse_args(argv)
+
+    with open('section.svg') as f:
+        lines = list(f)
+
+    #lines[1] = lines[1].replace('0 0 4176 3024', '2800 1900 3300 2100')
+    x = 1800
+    y = 1400
+    w = 550
+    h = 300
+    lines[1] = lines[1].replace('4176pt', f'{w}pt')
+    lines[1] = lines[1].replace('3024pt', f'{h}pt')
+    lines[1] = lines[1].replace(
+        '0 0 4176 3024',
+        f'{x} {y} {w} {h}')
+
+    lines = list(transform(lines))
+
+    with open('section2.svg', 'w') as f:
+        f.write(''.join(lines))
+
+def transform(lines):
+    for line in lines:
+        if '<use' in line:
+            fields = line.split()
+            assert fields[2].startswith('x=')
+            x = float(fields[2][3:-1])
+            if x > 2300:
+                continue
+        # if 'xlink:href="#glyph0-1' in line:
+        #     continue
+        yield line
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
