cd15b690476110c601a998abdf3eb526f76ff62c
Brandon Rhodes <brandon@rhodesmill.org>
2022-09-30 19:39:49 -0400

    Reference sheet for 2022

--- astronomy_events.py
+++ astronomy_events.py
@@ -3,6 +3,7 @@
 # python astronomy_events.py 36.0544 -112.1401 2019-04-23 2019-04-29 US/Arizona
 # python astronomy_events.py 36.0544 -112.1401 2020-09-27 2020-10-03 US/Arizona
 # ../astronomy_events.py 36.0544 -112.1401 2021-10-10 2021-10-17 US/Pacific
+# ../astronomy_events.py 36.0544 -112.1401 2022-10-02 2022-10-08 US/Arizona
 
 from __future__ import print_function
 
