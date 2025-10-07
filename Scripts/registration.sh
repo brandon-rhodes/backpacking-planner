9238ec9db7b264494a808c6bfd95f97424e006b9
Brandon Rhodes <brandon@rhodesmill.org>
2025-10-06 23:48:26 -0400

    Comment: remember how to use script for Chrome

--- Scripts/registration.sh
+++ Scripts/registration.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# For using Chrome to check campsite availability at:
+# https://www.recreation.gov/permits/4675337/registration/detailed-availability?date=2025-10-10
 
 set -ex
 
@@ -18,4 +21,3 @@ dt type 1
 dt key ctrl+f
 dt type classic
 dt key ctrl+Return
-dt key ctrl+f
